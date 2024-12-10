7ee3afdd3bf2983328fc581186ee99e822f3bb8d
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-12-10 09:02:42 +0100

    release v2.0.2

--- compatibility/ElectricTrains.lua
+++ compatibility/ElectricTrains.lua
@@ -7,7 +7,7 @@ local mk4 = data.raw.locomotive['locomotive-mk4']
 for _, entity in pairs({ mk3, mk4 }) do
   
   entity.reversing_power_modifier = 1.0
-  entity.energy_source.fuel_categories = {'electrical'}
+  entity.energy_source.fuel_categories = { 'electrical' }
   entity.energy_source.fuel_category = nil
   entity.energy_source.fuel_inventory_size = 1
   entity.energy_source.burnt_inventory_size = 1
@@ -19,7 +19,7 @@ for _, entity in pairs({ mk3, mk4 }) do
 end
 
 -- Update T3 recipe
-table.insert(data.raw.recipe['locomotive-mk3'].ingredients, {'deg-electric-locomotive', 1})
+table.insert(data.raw.recipe['locomotive-mk3'].ingredients, { type = 'item', name = 'deg-electric-locomotive', amount = 1 })
 
 -- Update tech dependencies
 table.insert(data.raw.technology['railway-3'].prerequisites, 'deg-electric-railway')

1e7b04155a3870b612f0aa481e777eb9bc20a218
saismee <43487101+saismee@users.noreply.github.com>
2026-06-24 03:42:51 +0100

    Hide ElectricTrains compat settings

--- settings.lua
+++ settings.lua
@@ -26,23 +26,27 @@ data:extend({
   }
 })
 
-data:extend({
-  {
-    type = "int-setting",
-    name = "bts-mk3_electric_capacity",
-    setting_type = "startup",
-    default_value = 500000000,
-    minimum_value = 250000000,
-    maximum_value = 100000000000,
-    order = "dd"
-  },
-  {
-    type = "int-setting",
-    name = "bts-mk4_electric_capacity",
-    setting_type = "startup",
-    default_value = 1000000000,
-    minimum_value = 250000000,
-    maximum_value = 100000000000,
-    order = "de"
-  },
-})
+
+-- ElectricTrains mod compatibility settings
+if mods['ElectricTrains'] then
+  data:extend({
+    {
+      type = "int-setting",
+      name = "bts-mk3_electric_capacity",
+      setting_type = "startup",
+      default_value = 500000000,
+      minimum_value = 250000000,
+      maximum_value = 100000000000,
+      order = "dd"
+    },
+    {
+      type = "int-setting",
+      name = "bts-mk4_electric_capacity",
+      setting_type = "startup",
+      default_value = 1000000000,
+      minimum_value = 250000000,
+      maximum_value = 100000000000,
+      order = "de"
+    },
+  })
+end
